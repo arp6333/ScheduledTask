93ec9ff8a4accee2ade5f4ff29a806df4e21b16d
Aniela Parobek <aniela.parobek@gmail.com>
2019-09-18 16:48:05 -0400

    Small update to script

--- Script.ps1
+++ Script.ps1
@@ -3,13 +3,10 @@
     This script will create a scheduled task or an 
     automatic task.
   .DESCRIPTION
-    Scheduled: Sets UAC Level to 2 every minute. To 
-    remove the scheduled task, go into Task
-    Scheduler and remove the task named "UACLevel".
+    Scheduled: Calls another script every hour.
 
-    Automatic: Sets UAC Level everytime it is modified
-    by another task. To remove the scheduled task, go into 
-    Task Scheduler and remove the task named "UACLevelAuto".
+    Automatic: Calls another script every time a
+    different task finishes.
   #>
 
 #Stores Script's Current Location
@@ -24,7 +21,7 @@ function Start-Main{
 #Creates Scheduled Task
 function Create-ScheduledTask{
     #Creates Scheduled Task Actions
-    $TaskAction1 = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument "-ExecutionPolicy Bypass -noprofile -command ""&{ start-process powershell -ArgumentList '-noprofile -file $sCurrDir\UACLevel.ps1' -verb RunAs}"""
+    $TaskAction1 = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument "-ExecutionPolicy Bypass -noprofile -command ""&{ start-process powershell -ArgumentList '-noprofile -file $sCurrDir\--ScriptToRun--.ps1' -verb RunAs}"""
             
     #Assigns Username
     $TaskUsername = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
@@ -39,10 +36,10 @@ function Create-ScheduledTask{
     $Task = New-ScheduledTask -Action $TaskAction1 -Principal $TaskUsername -Trigger $TaskTrigger -Settings $TaskSettings
         
     #Register Scheduled Task
-    Register-ScheduledTask "UACLevel" -InputObject $Task -Force
+    Register-ScheduledTask "--TaskName--" -InputObject $Task -Force
 
     #Configure Scheduled Task
-    $Task = Get-ScheduledTask -TaskName "UACLevel"
+    $Task = Get-ScheduledTask -TaskName "--TaskName--"
     #Omit Duration for infinite duration
     #$Task.Triggers.Repetition.Duration = "P1D"
     #Interval to repeat task (60M = 1 hour)
@@ -56,7 +53,7 @@ function Create-ScheduledTask{
 function Create-AutoTask{
     $taskName = "UACLevelAuto"
     $Path = 'PowerShell.exe'
-    $Arguments = "-ExecutionPolicy Bypass -noprofile -command ""&{ start-process powershell -ArgumentList '-noprofile -file $sCurrDir\UACLevel.ps1' -verb RunAs}"""
+    $Arguments = "-ExecutionPolicy Bypass -noprofile -command ""&{ start-process powershell -ArgumentList '-noprofile -file $sCurrDir\--ScriptToRun--.ps1' -verb RunAs}"""
 
     $Service = new-object -ComObject ("Schedule.Service")
     $Service.Connect()
@@ -70,7 +67,7 @@ function Create-AutoTask{
     $Trigger = $Triggers.Create(0)
     $Trigger.Enabled = $true
     $Trigger.Id = '102'
-    $Trigger.Subscription = "<QueryList><Query Id=""0"" Path=""Microsoft-Windows-TaskScheduler/Operational""><Select Path=""Microsoft-Windows-TaskScheduler/Operational"">*[EventData[@Name='TaskSuccessEvent'][Data[@Name='TaskName']='\Microsoft\Windows\DeviceDirectoryClient\RegisterDevicePolicyChange']]</Select></Query></QueryList>"
+    $Trigger.Subscription = "<QueryList><Query Id=""0"" Path=""Microsoft-Windows-TaskScheduler/Operational""><Select Path=""Microsoft-Windows-TaskScheduler/Operational"">*[EventData[@Name='TaskSuccessEvent'][Data[@Name='TaskName']='--TaskNameHere--']]</Select></Query></QueryList>"
     $Action = $TaskDefinition.Actions.Create(0)
     $Action.Path = $Path
     $action.Arguments = $Arguments
